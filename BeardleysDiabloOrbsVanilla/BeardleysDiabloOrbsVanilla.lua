11a97aa9b7ebd77dd7c5b1f9c3971e2b5c229319
Kulturnilpferd <37019912+Kulturnilpferd@users.noreply.github.com>
2019-06-16 03:52:02 +0200

    Update BeardleysDiabloOrbsVanilla.lua

--- BeardleysDiabloOrbsVanilla/BeardleysDiabloOrbsVanilla.lua
+++ BeardleysDiabloOrbsVanilla/BeardleysDiabloOrbsVanilla.lua
@@ -28,26 +28,25 @@ function BDOV_OnLoad()
 	this:RegisterEvent("LOOT_CLOSED")
 end
 
-local function addArtworkFrame(file,parentFrame,frameName,offsetX,offsetY,height,width)
+local function addArtworkFrame(file,parentFrame,frameName,FrameStrata,offsetX,offsetY,height,width)
 	local artworkFrame = CreateFrame("Frame",frameName,parentFrame)
 	artworkFrame:SetPoint("BOTTOM",offsetX,offsetY)
+	artworkFrame:SetFrameStrata(FrameStrata)
 	artworkFrame:SetHeight(height)
 	artworkFrame:SetWidth(width)
 	artworkFrame.texture = artworkFrame:CreateTexture(nil,"OVERLAY")
 	artworkFrame.texture:SetTexture(file)
 	artworkFrame.texture:SetAllPoints(artworkFrame)
+	
 	return artworkFrame
 end
 
 local function createArtwork()
-	actionbarBackground = addArtworkFrame(images.."bar3.tga",UIParent,nil,1,-4,127,491)
-	actionbarBackground:SetFrameStrata("LOW")	
+	actionbarBackground = addArtworkFrame(images.."bar3.tga",UIParent,nil,"LOW",1,-4,127,491)
 	
-	leftArtwork = addArtworkFrame(images.."leftArtwork.tga",UIParent,nil,-325,0,200,200)
-	leftArtwork:SetFrameStrata("HIGH")	
+	leftArtwork = addArtworkFrame(images.."leftArtwork.tga",UIParent,nil,"HIGH",-325,0,200,200)
 	
-	rightArtwork = addArtworkFrame(images.."rightArtwork.tga",UIParent,nil,325,0,200,200)
-	rightArtwork:SetFrameStrata("HIGH")
+	rightArtwork = addArtworkFrame(images.."rightArtwork.tga",UIParent,nil,"HIGH",325,0,200,200)
 end
 
 local function changeActionBar()
